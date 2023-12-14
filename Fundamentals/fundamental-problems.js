1e973a57d28f4d96af74a019d2323596cbdc7da7
spaceTony9 <bilokon.fw7@gmail.com>
2023-12-14 20:41:08 +0100

    upd

--- Fundamentals/fundamental-problems.js
+++ Fundamentals/fundamental-problems.js
@@ -210,3 +210,91 @@
 // console.log(countPositivesSumNegatives([]));
 // console.log(countPositivesSumNegatives([null]));
 
+// convert number to string, make an array from the string and put to square each number in the string
+
+// function squareDigits(num) {
+//   num += "";
+//   num = num
+//     .split("")
+//     .map((number) => number * number)
+//     .join("");
+//   return Number(num);
+// }
+
+// console.log(squareDigits(4343));
+
+// function isIsogram(str) {
+//     //...
+//     str = str.split('');
+//     for (let i = 0; i < str.length; i++) {
+//       for (let j = i + 1; j < str.length; j++) {
+//           if (str[i] === str[j]) {
+//               return false;
+//         }
+//       }
+//     }
+//     return true;
+// }
+
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach((number) => (number > value ? filteredNumbers.push(number) : []));
+
+//   return filteredNumbers;
+// };
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+
+// console.log("hello world");
+
+
+// function changeEven(numbers, value) {
+//   const finalArray = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 === 0) {
+//       finalArray.push(numbers[i] + value);
+//     }
+//     else {
+//       finalArray.push(numbers[i]);
+//     }
+//   }
+//   return finalArray;
+// }
+
+
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10)); 
+// // [12, 18, 3, 7, 14, 16]`
+
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  {
+    title: "The Dreams in the Witch House",
+    author: "Howard Lovecraft",
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
+
+const names = books
+  .filter((book) => book.rating > MIN_BOOK_RATING)
+  .map((book) => book.author)
+  .sort((a, b) => a.localeCompare(b));
+
+  console.log(names);
